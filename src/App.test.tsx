58b41127ae5c085ca8244e4618fe5fd5c575383b
Morten Jensen <mj.95dk@gmail.com>
2024-10-14 19:36:57 +0200

    Fixes test setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,46 +1,48 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import App from "./App";
 import userEvent from "@testing-library/user-event";
-import { expect, test } from "vitest";
+import { expect, it, describe } from "vitest";
 
-test("renders a card selector", () => {
-  render(<App />);
-  const cardSelectorElement = screen.getByTestId("card-selector");
-  expect(cardSelectorElement).toBeInTheDocument();
-});
+describe("<App />", () => {
+  it("renders a card selector", () => {
+    render(<App />);
+    const cardSelectorElement = screen.getByTestId("card-selector");
+    expect(cardSelectorElement).toBeInTheDocument();
+  });
 
-test("renders a big card", () => {
-  render(<App />);
-  const bigCardElement = screen.getByTestId("big-card");
-  expect(bigCardElement).toBeInTheDocument();
-});
+  it("renders a big card", () => {
+    render(<App />);
+    const bigCardElement = screen.getByTestId("big-card");
+    expect(bigCardElement).toBeInTheDocument();
+  });
 
-test("renders a card with a number", () => {
-  render(<App />);
-  const cardElement = screen.getByText("8");
-  expect(cardElement).toBeInTheDocument();
-});
+  it("renders a card with a number", () => {
+    render(<App />);
+    const cardElement = screen.getByText("8");
+    expect(cardElement).toBeInTheDocument();
+  });
 
-test("renders a card with a symbol", () => {
-  render(<App />);
-  const cardElement = screen.getByText("∞");
-  expect(cardElement).toBeInTheDocument();
-});
+  it("renders a card with a symbol", () => {
+    render(<App />);
+    const cardElement = screen.getByText("∞");
+    expect(cardElement).toBeInTheDocument();
+  });
 
-test("renders a card with a question mark", () => {
-  render(<App />);
-  const cardElement = screen.getByText("?");
-  expect(cardElement).toBeInTheDocument();
-});
+  it("renders a card with a question mark", () => {
+    render(<App />);
+    const cardElement = screen.getByText("?");
+    expect(cardElement).toBeInTheDocument();
+  });
 
-test("when a card is clicked, the big card is flipped", async () => {
-  render(<App />);
-  const cardElement = screen.getByText("8");
-  userEvent.click(cardElement);
-  const bigCardElement = await screen.findByTestId("big-card");
-  expect(bigCardElement).not.toHaveClass("flipped");
-  userEvent.click(bigCardElement);
-  await waitFor(() => {
-    expect(bigCardElement).toHaveClass("flipped");
+  it("when a card is clicked, the big card is flipped", async () => {
+    render(<App />);
+    const cardElement = screen.getByText("8");
+    userEvent.click(cardElement);
+    const bigCardElement = await screen.findByTestId("big-card");
+    expect(bigCardElement).not.toHaveClass("flipped");
+    userEvent.click(bigCardElement);
+    await waitFor(() => {
+      expect(bigCardElement).toHaveClass("flipped");
+    });
   });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,18 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   // depending on your application, base can also be "/"
-  base: "planning-poker",
+  base: "/planning-poker/",
   plugins: [react({ jsxRuntime: "automatic" })],
   server: {
     // this ensures that the browser opens upon server start
     open: true,
     // this sets a default port to 3000
     port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.ts"],
+    include: ["src/**/*.test.tsx"],
   },
 });
