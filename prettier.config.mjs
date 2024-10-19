cf4777fa5a8de66847260de3b98b10c390fd8a8c
Morten Jensen <mj.95dk@gmail.com>
2024-10-19 21:12:43 +0200

    Adds Sass and prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 100,
+  trailingComma: 'all',
+  singleQuote: true,
+  semi: false,
+  arrowParens: 'always',
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,27 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   // depending on your application, base can also be "/"
-  base: "/planning-poker/",
-  plugins: [react({ jsxRuntime: "automatic" })],
+  base: '/planning-poker/',
+  plugins: [react({ jsxRuntime: 'automatic' })],
   server: {
     // this ensures that the browser opens upon server start
     open: true,
     // this sets a default port to 3000
     port: 3000,
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
   test: {
     globals: true,
-    environment: "jsdom",
-    setupFiles: ["./src/setupTests.ts"],
-    include: ["src/**/*.test.tsx"],
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+    include: ['src/**/*.test.tsx'],
   },
-});
+})
