4b155021779d754fec338ae52942c3d503bdab2d
Morten Jensen <mj.95dk@gmail.com>
2024-10-19 22:28:42 +0200

    Adds card selection panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import { SelectedCard } from './SelectedCard'
+import { CardSelectorPanel } from './CardSelectorPanel'
 
 // Fibonacci sequence
 const fibonacciSequence = [0, '½', 1, 2, 3, 5, 8, 13, 21, '∞', '?']
@@ -9,23 +10,11 @@ export type Card = (typeof fibonacciSequence)[number]
 function App() {
   const [selectedCard, setSelectedCard] = useState<Card>('?')
 
-  const handleCardSelection = (card: Card) => () => {
-    setSelectedCard(card)
-  }
-
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <h1>Planning Poker</h1> */}
-        <SelectedCard selectedCard={selectedCard} />
-        <div className="card-container" data-testid="card-selector">
-          {fibonacciSequence.map((number) => (
-            <button key={number} className="card" onClick={handleCardSelection(number)}>
-              {number}
-            </button>
-          ))}
-        </div>
-      </header>
+      {/* <h1>Planning Poker</h1> */}
+      <SelectedCard selectedCard={selectedCard} />
+      <CardSelectorPanel onCardSelection={setSelectedCard} />
     </div>
   )
 }
---
+++ src/CardSelectorPanel/CardSelectorPanel.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import type { Card } from '../App'
+import './CardSelectorPanel.scss'
+
+const fibonacciSequence = [0, '½', 1, 2, 3, 5, 8, 13, 21, '∞', '?']
+
+export const CardSelectorPanel = ({
+  onCardSelection,
+}: {
+  onCardSelection: (card: Card) => void
+}) => {
+  const [fullSize, setFullSize] = useState(false)
+
+  const cardSelect = (card: Card) => {
+    onCardSelection(card)
+    setFullSize(false)
+  }
+
+  return (
+    <div className={`card-selector-panel ${fullSize ? 'full-size' : 'shrink'}`}>
+      <div className="top-panel">
+        <button className="expand-btn" onClick={() => setFullSize(!fullSize)}>
+          {fullSize ? '↓' : '↑'}
+        </button>
+      </div>
+      {fibonacciSequence.map((card) => (
+        <button key={card} className="card" onClick={() => cardSelect(card)}>
+          {card}
+        </button>
+      ))}
+    </div>
+  )
+}
