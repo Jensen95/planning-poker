f3001dc63b2ae3e1fbff99d5cc99edc970423ead
Morten Jensen <mj.95dk@gmail.com>
2024-10-24 05:20:55 +0200

    Adds shake to flip card

--- src/SelectedCard/SelectedCard.tsx
+++ src/SelectedCard/SelectedCard.tsx
@@ -1,11 +1,17 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import type { Card } from '../App'
 
 import './SelectedCard.scss'
+import { useDebounce, useMotion, useVibrate } from 'react-use'
 
 export const SelectedCard = ({ selectedCard }: { selectedCard: Card }) => {
   const [isFlipped, setIsFlipped] = useState(false)
+
   const [shownCards, setShownCards] = useState<Card[]>([])
+
+  useShake(() => {
+    setIsFlipped((prev) => !prev)
+  })
 
   useEffect(() => {
     setIsFlipped(false)
@@ -41,6 +47,48 @@ export const SelectedCard = ({ selectedCard }: { selectedCard: Card }) => {
       </div>
     </div>
   )
+}
+
+const getMaxAcceleration = (acceleration: {
+  x: number | null
+  y: number | null
+  z: number | null
+}): number => {
+  let max = 0
+  for (const key of ['x', 'y', 'z'] as const) {
+    const value = Math.abs(acceleration?.[key] ?? 0)
+    if (value > max) max = value
+  }
+  return max
+}
+
+const useShake = (onShake: () => void) => {
+  const test = useMotion()
+  const [shaking, setShaking] = useState(false)
+  const [vibrate, setVibrate] = useState(false)
+  useVibrate(vibrate, [100, 100, 100, 100], false)
+  const [, cancelDebounce] = useDebounce(
+    () => {
+      if (shaking) {
+        onShake()
+        setVibrate(true)
+        setShaking(false)
+      }
+    },
+    300,
+    [shaking],
+  )
+  // useVibrate(false,)
+  const max = getMaxAcceleration(test?.acceleration)
+  if (max > 15) {
+    // setVibrate(true)
+    if (!shaking) {
+      setShaking(true)
+      setVibrate(false)
+    }
+  }
+
+  return null
 }
 
 const FlippedCard = ({ card }: { card: Card }) => {
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default defineConfig({
   // depending on your application, base can also be "/"
   base: '/planning-poker/',
-  plugins: [react({ jsxRuntime: 'automatic' })],
+  plugins: [react({ jsxRuntime: 'automatic' }), basicSsl()],
   server: {
     // this ensures that the browser opens upon server start
     open: true,
     // this sets a default port to 3000
     port: 3000,
+    host: true,
+    https: true,
   },
   css: {
     preprocessorOptions: {
